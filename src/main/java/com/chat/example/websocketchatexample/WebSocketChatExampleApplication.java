6ec04233a9f9b42f5cdfd8b6d0954a79c1730a80
Boss <wbc123454321@gmail.com>
2023-12-28 12:32:42 +0300

    Web socket example

---
+++ src/main/java/com/chat/example/websocketchatexample/WebSocketChatExampleApplication.java
@@ -0,0 +1,13 @@
+package com.chat.example.websocketchatexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketChatExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketChatExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chat/example/websocketchatexample/WebSocketChatExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.example.websocketchatexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
